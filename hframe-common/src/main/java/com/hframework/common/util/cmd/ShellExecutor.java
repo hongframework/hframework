0715d604ecc4a018000a4c240be51a14e790821a
“zhangquanhong <zhangquanhong@ucfgroup.com>
2017-10-18 15:16:23 +0800

    shell调用时，增加异步调用处理。

--- hframe-common/src/main/java/com/hframework/common/util/cmd/ShellExecutor.java
+++ hframe-common/src/main/java/com/hframework/common/util/cmd/ShellExecutor.java
@@ -10,6 +10,57 @@ import java.io.InputStreamReader;
  */
 public class ShellExecutor {
 
+    public static Process exeCmdAsync(String commandStr) {
+        BufferedReader br = null;
+        try {
+            Process p = Runtime.getRuntime().exec(commandStr);
+            return p;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (br != null)
+            {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        throw new RuntimeException("脚本启动失败！");
+    }
+
+
+    public static String getProcessResult(Process p) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(p.getInputStream(),"GBK"));
+            String line = null;
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            System.out.println(sb.toString());
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (br != null)
+            {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
     public static void exeCmd(String commandStr) {
         BufferedReader br = null;
         try {
@@ -41,6 +92,9 @@ public class ShellExecutor {
         String commandStr = "ping www.taobao.com";
         //String commandStr = "ipconfig";
 //        exeCmd(commandStr);
-        exeCmd("D:\\my_workspace\\hframe-shop\\build\\compile.bat");
+        Process process = exeCmdAsync("D:\\my_workspace\\hframe-shop\\build\\compile.bat");
+        process.destroy();
+        System.out.println(process.exitValue());
+        System.out.println(getProcessResult(process));
     }
 }
